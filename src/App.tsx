e36229735144aced79d72e55ae37d35bb9d68e5f
Aditya Tapshalkar <aditya.taps@gmail.com>
2024-11-05 20:46:20 -0500

    added placeholder pics

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import placeHolderLogo from "./assets/placeholder-logo.jpg";
+
 import "./App.css";
+
+const CDN_URL: String = import.meta.env.VITE_CDN_URL;
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -29,9 +32,53 @@ function App() {
       {/*<p className="read-the-docs">*/}
       {/*  Click on the Vite and React logos to learn more*/}
       {/*</p>*/}
-
       <div>Under construction...</div>
       <img src={placeHolderLogo} alt="placeholder" style={{ width: "100%" }} />
+      <img
+        src={`${CDN_URL}/farm-pics/Back of Structure - Rear view of property featuring a sunroom and a lawn.jpg`}
+        alt="Back of Structure - Rear view of property featuring a sunroom and a lawn.jpg"
+        style={{ width: "100%" }}
+      />
+      <img
+        src={`${CDN_URL}/farm-pics/IMG_0008.JPG`}
+        alt={"Sukhi on tractor"}
+        style={{ width: "100%" }}
+      />
+      <img
+        src={`${CDN_URL}/farm-pics/IMG_0223.JPG`}
+        alt={"Sukhi"}
+        style={{ width: "100%" }}
+      />
+      <img
+        src={`${CDN_URL}/farm-pics/IMG_0397.JPG`}
+        alt={"Sukhi at sunset"}
+        style={{ width: "100%" }}
+      />
+      <img
+        src={`${CDN_URL}/farm-pics/IMG_0714.JPG`}
+        alt={"Sukhi farming"}
+        style={{ width: "100%" }}
+      />
+      <img
+        src={`${CDN_URL}/farm-pics/IMG_0722.JPG`}
+        alt={"Persimmon"}
+        style={{ width: "100%" }}
+      />{" "}
+      <img
+        src={`${CDN_URL}/farm-pics/IMG_0731.JPG`}
+        alt={"Sukhi + Suren"}
+        style={{ width: "100%" }}
+      />{" "}
+      <img
+        src={`${CDN_URL}/farm-pics/IMG_7058.JPG`}
+        alt={"Berries"}
+        style={{ width: "100%" }}
+      />{" "}
+      <img
+        src={`${CDN_URL}/farm-pics/IMG_7059.JPG`}
+        alt={"Berries"}
+        style={{ width: "100%" }}
+      />
     </>
   );
 }
